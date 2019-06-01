62e3b1a886c064167937b39be142bdf333af0186
DanikKaragodin <51264803+DanikKaragodin@users.noreply.github.com>
2019-06-01 15:54:35 +0300

    vlad_pulka

---
+++ JavaFXApplication4/src/javafxapplication4/FXMLDocumentController.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxapplication4;
+
+import javafx.fxml.FXML;
+
+/**
+ *
+ * @author PC
+ */
+class FXMLDocumentController {
+      @FXML
+     public static double x, y;
+    
+}
